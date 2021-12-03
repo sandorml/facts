a7aac4c6528af0133171dfca6b2df9016a0e932b
Sandor Martin Leyva <sandormartin37@gmail.com>
2021-12-03 09:31:50 -0500

    Add Fact component

---
+++ src/components/Fact.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card } from 'antd';
+import styled from 'styled-components';
+import { Fact } from '../types/Fact';
+
+
+const CardP = styled(Card)`
+box-shadow: 0px 0px 4px 0px #88888891;
+border-radius: 8px;
+margin: 5px;
+width: 250px;
+`;
+
+const Fact = (props: Fact) => {
+    return (
+        <CardP bordered={false}>
+            <p>{props.text}</p>
+        </CardP>);
+}
+
+export default Fact
+
---
+++ src/types/Fact.ts
@@ -0,0 +1,4 @@
+
+export type Fact = {
+    text: string,
+};
