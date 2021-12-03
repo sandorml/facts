5ad8a9dd2791b481d2cf887de062f515e04ccbd8
Sandor Martin Leyva <sandormartin37@gmail.com>
2021-12-03 11:48:26 -0500

    Add test to FactCard

---
+++ src/components/FactCard.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import FactCard from './FactCard';
+
+test('<FactCard/> render text', () => {
+  const { getByText } = render(<FactCard text={"testing"} />);
+  const element = getByText(/testing/i);
+  expect(element).toBeInTheDocument();
+});
